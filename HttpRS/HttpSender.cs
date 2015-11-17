07d37ada6281280d39bf7be173f62b5293c3aed9
psliurt <psliurt@gmail.com>
2015-11-17 18:30:30 +0800

    a class that contains http response message

--- HttpRS/HttpSender.cs
+++ HttpRS/HttpSender.cs
@@ -58,38 +58,6 @@ namespace HttpRS
             return _responseHeader;
         }
 
-        //private void ParseContentType(string contentTypeValue)
-        //{
-        //    string[] contentTypes = contentTypeValue.Split(',');
-        //    foreach (string ct in contentTypes)
-        //    {
-        //        if (!string.IsNullOrEmpty(ct))
-        //        {
-        //            string[] mediaTypeNencoding = ct.Trim().Split(';');
-        //            string mediaType = mediaTypeNencoding[0].Trim();
-        //            if (mediaTypeNencoding.Length > 1 &&
-        //                !string.IsNullOrEmpty(mediaTypeNencoding[1]))
-        //            {
-        //                string encoding = mediaTypeNencoding[1].Trim();
-
-        //                if (encoding.Trim().ToLower().Contains("utf-8"))
-        //                {
-        //                    SetResponseEncoding(Encoding.UTF8);
-        //                }
-        //                if (encoding.Trim().ToLower().Contains("big5"))
-        //                {
-        //                    SetResponseEncoding(Encoding.Default);
-        //                }
-        //            }
-        //            else
-        //            {
-        //                SetResponseEncoding(Encoding.Default);
-        //            }
-                    
-        //        }
-        //    }
-        //}
-
         public String SendRequest(String method, String body, HttpHeaderList header)
         {
             _request = (HttpWebRequest)WebRequest.Create(_uri);
@@ -228,6 +196,156 @@ namespace HttpRS
                 _response.Close();
             }
             return responseString;
+        }
+
+
+        public ResponseResult SendRequest(HttpRequestMethod method, String body, HttpHeaderList header)
+        {
+            ResponseResult rspResult = new ResponseResult();
+            _request = (HttpWebRequest)WebRequest.Create(_uri);
+            _request.Method = method.ToString().ToUpper();
+            _request = HeaderHelper.BuildRequestHeader(_request, header);
+
+            //如果有要利用http body傳送的資料，才會用outputStream來送資料
+            if (method.ToString().ToUpper() != "GET" &&
+                !string.IsNullOrEmpty(body))
+            {
+                Stream outStream = null;
+                try
+                {
+                    outStream = _request.GetRequestStream();
+                    byte[] outData = _requestStreamEncoding.GetBytes(body);
+                    outStream.Write(outData, 0, outData.Length);
+                }
+                catch (WebException we)
+                {
+                    if (we.Response == null)
+                    {
+                        rspResult.ErrorMsg = we.Status.ToString();
+                        rspResult.IsResultError = true;
+                        return rspResult;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (NotSupportedException nse)
+                {
+
+                }
+                catch (ProtocolViolationException pve)
+                {
+
+                }
+                catch (InvalidOperationException ivoe)
+                {
+
+                }
+                catch (Exception e)
+                {
+                    //log or do some process
+                }
+                finally
+                {
+                    if (outStream != null)
+                    {
+                        outStream.Close();
+                    }
+                }
+            }
+
+            try
+            {
+                _response = (HttpWebResponse)_request.GetResponse();
+            }
+            catch (WebException we)
+            {
+                _response = (HttpWebResponse)we.Response;
+                _responseHeader = HeaderHelper.ParseResponseHeader(_response);                
+
+                SetResponseEncoding(Encoding.GetEncoding(_response.CharacterSet));
+
+                Stream inputs = _response.GetResponseStream();
+                string respBody = string.Empty;
+                using (StreamReader sr = new StreamReader(inputs, Encoding.GetEncoding(_response.CharacterSet)))
+                {
+                    respBody = sr.ReadToEnd();
+                }
+                string statusMsg = string.Format("({0})\r\n{1}", _response.StatusDescription, respBody);
+
+                rspResult.Headers = _responseHeader;
+                rspResult.IsResultError = true;
+                rspResult.ResponseBody = respBody;
+                rspResult.ErrorMsg = statusMsg;
+                rspResult.StatusMsg = _response.StatusCode.ToString();
+                return rspResult;
+            }
+            catch (NotSupportedException nse)
+            {
+
+            }
+            catch (ProtocolViolationException pve)
+            {
+
+            }
+            catch (InvalidOperationException ivoe)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            _responseHeader = HeaderHelper.ParseResponseHeader(_response);
+
+            SetResponseEncoding(Encoding.GetEncoding(_response.CharacterSet));
+
+            Stream inputStream = null;
+            String responseString = string.Empty;
+            try
+            {
+                inputStream = _response.GetResponseStream();
+                using (StreamReader sr = new StreamReader(inputStream, Encoding.GetEncoding(_response.CharacterSet)))
+                {
+                    responseString = sr.ReadToEnd();
+                }
+            }
+            catch (ObjectDisposedException ode)
+            {
+
+            }
+            catch (ProtocolViolationException pve)
+            {
+            }
+            catch (OutOfMemoryException oome)
+            {
+
+            }
+            catch (IOException ioe)
+            {
+
+            }
+            catch (Exception e)
+            {
+                //log some error message or do some process;
+            }
+            finally
+            {
+                if (inputStream != null)
+                {
+                    inputStream.Close();
+                }
+                _response.Close();
+            }            
+
+            rspResult.ResponseBody = responseString;
+            rspResult.IsResultError = false;
+            rspResult.Headers = _responseHeader;
+            rspResult.StatusMsg = _response.StatusCode.ToString();
+
+            return rspResult;
         }
     }
 
---
+++ HttpRS/ResponseResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpRS
+{
+    public class ResponseResult
+    {
+        private string _statusMsg;
+
+        public bool IsResultError { get; set; }
+        public string ErrorMsg { get; set; }
+        public string ResponseBody { get; set; }
+        public int StatusCode { get; set; }        
+        public string OtherNote { get; set; }
+        public HttpHeaderList Headers { get; set; }
+
+        public string StatusMsg 
+        { 
+            get 
+            {
+                return _statusMsg;
+            }
+            set 
+            {
+                SetStatusCode(value);
+                _statusMsg = value;
+            }
+        }
+
+        private void SetStatusCode(string statusMsg)
+        {
+            switch (statusMsg)
+            { 
+                case "Accepted":
+                    StatusCode = 202;
+                    break;
+                default:
+                    StatusCode = 200;
+                    break;
+            }
+        }
+    }
+}
